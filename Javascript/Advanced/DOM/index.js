d92c94e1ccda9cd53e1971cb3ad02be36a44fcba
DeepTechIndia <deeptechsoln@gmail.com>
2024-10-11 09:19:06 +0530

    DOM

---
+++ Javascript/Advanced/DOM/index.js
@@ -0,0 +1,34 @@
+//DOM => Document object Model
+
+/* 1. selecting element
+2. chnag content
+3.change styles
+4. add or remove the elements 
+5. attributes
+6. handling events*/
+
+//How to select the element
+
+//getElements By ID
+/* const header = document.getElementById("header");
+console.log(header); */
+
+//get elements by className
+/* const paragraphs = document.getElementsByClassName("paragraph");
+console.log(paragraphs); */
+
+// getElementsByTagName
+/* const paragraphs = document.getElementsByTagName("p");
+console.log(paragraphs); */
+
+//querySelector
+
+/* const firstParagraph = document.querySelector(".paragraph");
+console.log(firstParagraph);
+
+console.log(document.querySelector("#header")); */
+
+//querySelectorAll
+
+/* const allParagraphs = document.querySelectorAll(".paragraph");
+console.log(allParagraphs); */
